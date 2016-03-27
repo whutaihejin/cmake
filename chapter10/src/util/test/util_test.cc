45954d51fc96f3ece9909b0d6b3970970862b703
taihejin <taihejin2008@126.com>
2016-03-27 22:34:41 +0800

    learning & fighting

--- chapter10/src/util/test/util_test.cc
+++ chapter10/src/util/test/util_test.cc
@@ -13,13 +13,20 @@ TEST (MaxTest, PositiveZero) {
   EXPECT_EQ (5, max(0, 5));
 }
 
-TEST (MaxTest, Negative) {
+TEST (MaxTest, NegativeZero) {
   EXPECT_EQ (0, max(-1, 0));
   EXPECT_EQ (0, max(0, -1));
   EXPECT_EQ (-1, max(-5, -1));
   EXPECT_EQ (-1, max(-1, -5));
   EXPECT_EQ (-100, max(-100, -1000));
   EXPECT_EQ (-100, max(-1000, -100));
+}
+
+TEST (DISABLED_MaxTest, PositiveNegative) {
+  EXPECT_EQ (2, max(-5, 2));
+  EXPECT_EQ (2, max(2, -5));
+  EXPECT_EQ (9, max(-1, 9));
+  EXPECT_EQ (9, max(9, -1));
 }
 
 int main(int argc, char* argv[]) {
